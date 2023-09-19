5cc02552ce84587a969efbde9fc2646129302637
KipiX <peev.vadim@gmail.com>
2023-09-19 18:17:15 +0900

    update

--- main.py
+++ main.py
@@ -3,211 +3,166 @@ import requests
 import telebot
 import datetime
 import time
-from telebot import types
 
 
-bot = telebot.TeleBot('5946513884:AAGkl2MPaJ5sK0BRYsbWkY6PV23JJBF1-aU')
-API = '709e3cbe105d05f7bfdb27403929d3bc'
+class WeatherBot:
+    def __init__(self, bot_token, openweathermap_api_key):
+        self.bot = telebot.TeleBot(bot_token)
+        self.API = openweathermap_api_key
 
-# переводчик
-weather_mapping = {
-    'clear sky': 'ясно',
-    'few clouds': 'малооблачно',
-    'scattered clouds': 'рассеянные облака',
-    'overcast clouds': 'затянуто облаками',
-    'broken clouds': 'облачно',
-    'shower rain': 'ливень',
-    'rain': 'дождь',
-    'thunderstorm': 'гроза',
-    'snow': 'снег',
-    'mist': 'туман',
-    'haze': 'лёгкий туман',
-    'light rain': 'небольшой дождь',
-    'light intensity shower rain': 'ливень с интенсивностью света',
-    'fog': 'туман',
-    'heavy intensity rain': 'сильный дождь',
-}
+        # перевод
+        with open('weather_data.json', 'r', encoding='utf-8') as json_file:
+            self.weather_mapping = json.load(json_file)
 
-city_weather_data = {}
-# начало
-last_city = None
-scheduled_message = None
-sending_weather = False
+        self.city_weather_data = {}
+        self.last_city = None
+        self.scheduled_message = None
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Привет! Я бот погоды. Чтобы узнать погоду, просто напиши название города.")
+        # Словарь для отслеживания подключенных пользователей и состояния их рассылки
+        self.subscribed_users = {}
 
+        # Атрибут для отслеживания состояния рассылки
+        self.sending_weather = False
 
-def get_weather_for_date(city, date):
-    res = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API}&units=metric')
-    if res.status_code == 200:
-        data = json.loads(res.text)
-        # найти прогноз погоды (3 часа)
-        for item in data["list"]:
-            item_datetime = datetime.datetime.strptime(item["dt_txt"], "%Y-%m-%d %H:%M:%S")
-            if item_datetime.date() == date.date():
-                return item
-    return None
+        # Инициализируем клавиатуру для кнопок
+        self.markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+        self.markup.add(telebot.types.KeyboardButton("Присылать каждый час"))
+        # self.markup.add(telebot.types.KeyboardButton("Стоп"))
+        self.markup.add(telebot.types.KeyboardButton("Погода на 3 дня"))
+        self.markup.add(telebot.types.KeyboardButton("Завтра"))
 
+        @self.bot.message_handler(commands=['start'])
+        def start(message):
+            self.bot.send_message(message.chat.id, "Привет! Я бот погоды. Чтобы узнать погоду, просто напиши название города.")
 
-@bot.message_handler(func=lambda message: message.text.lower() == "завтра")
-def get_weather_tomorrow(message):
-    global last_city
-    # завтра
-    if last_city:
-        res = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?q={last_city}&appid={API}&units=metric')
+        @self.bot.message_handler(func=lambda message: message.text.lower() == "завтра")
+        def get_weather_tomorrow(message):
+            if self.last_city:
+                tomorrow_weather_info = self.get_weather_for_date(self.last_city,
+                                                                  datetime.datetime.now() + datetime.timedelta(days=1))
+                if tomorrow_weather_info:
+                    self.bot.send_message(message.chat.id, tomorrow_weather_info, reply_markup=self.markup)
+                else:
+                    self.bot.send_message(message.chat.id,
+                                          f'Не удалось получить прогноз погоды на завтра для {self.last_city}... 🥺')
+            else:
+                self.bot.send_message(message.chat.id, 'Вы еще не указали город. Сначала укажите город.')
+
+        # погода на 3 дня
+        @self.bot.message_handler(func=lambda message: message.text.lower() == "погода на 3 дня")
+        def get_weather_three_days(message):
+            if self.last_city:
+                city = self.last_city
+                current_date = datetime.datetime.now()
+                tomorrow_date = current_date + datetime.timedelta(days=1)
+                day_after_tomorrow_date = current_date + datetime.timedelta(days=2)
+                third_day_date = current_date + datetime.timedelta(days=3)
+
+                tomorrow_weather_info = self.get_weather_for_date(city, tomorrow_date)
+                day_after_tomorrow_weather_info = self.get_weather_for_date(city, day_after_tomorrow_date)
+                third_day_weather_info = self.get_weather_for_date(city, third_day_date)
+
+                if tomorrow_weather_info and day_after_tomorrow_weather_info and third_day_weather_info:
+                    weather_info = (f'🏙️Погода в {city.capitalize()}:\n\n'
+                                    f'📅Завтра:\n'
+                                    f'{tomorrow_weather_info}\n\n'
+                                    f'📅Послезавтра:\n'
+                                    f'{day_after_tomorrow_weather_info}\n\n'
+                                    f'📅Через 3 дня:\n'
+                                    f'{third_day_weather_info}\n')
+
+                    self.bot.send_message(message.chat.id, weather_info, reply_markup=self.markup)
+                else:
+                    self.bot.send_message(message.chat.id,
+                                          f'Не удалось получить прогноз погоды на 3 дня для {city}... 🥺')
+            else:
+                self.bot.send_message(message.chat.id, 'Вы еще не указали город. Пожалуйста, укажите город.')
+
+        # рассылка каждый час1
+        @self.bot.message_handler(func=lambda message: True)
+        def handle_city(message):
+            if message.text.lower() == "стоп":
+                if self.sending_weather:
+                    self.sending_weather = False
+                    if message.chat.id in self.subscribed_users:
+                        self.subscribed_users[message.chat.id] = False
+                    # клава
+                    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+                    markup.add(telebot.types.KeyboardButton("Присылать каждый час"))
+                    markup.add(telebot.types.KeyboardButton("Погода на 3 дня"))
+                    markup.add(telebot.types.KeyboardButton("Завтра"))
+                    self.bot.send_message(message.chat.id, "Рассылка погоды остановлена.", reply_markup=markup)
+                else:
+                    self.bot.send_message(message.chat.id, "Рассылка погоды уже остановлена.")
+            elif message.text.lower() == "присылать каждый час":
+                if self.sending_weather:
+                    self.bot.send_message(message.chat.id, "Рассылка уже запущена!")
+                else:
+                    self.sending_weather = True
+                    # Уберите клавиатуру с кнопкой "Присылать каждый час" и добавьте кнопку "Стоп"
+                    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+                    markup.add(telebot.types.KeyboardButton("Стоп"))
+                    markup.add(telebot.types.KeyboardButton("Погода на 3 дня"))
+                    markup.add(telebot.types.KeyboardButton("Завтра"))
+                    self.bot.send_message(message.chat.id,
+                                          "Рассылка погоды запущена. Погода будет присылаться каждый час.",
+                                          reply_markup=markup)
+                    self.subscribed_users[message.chat.id] = True
+                    self.send_weather_periodically(message.chat.id)
+            else:
+                self.last_city = message.text.lower()
+                self.send_weather(message.chat.id, self.last_city)
+
+    # общая погда (на сегодня)
+    def get_weather_for_date(self, city, date):
+        res = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={self.API}&units=metric')
+
         if res.status_code == 200:
             data = json.loads(res.text)
-            tomorrow_weather = data["list"][8]
-            temp = tomorrow_weather["main"]["temp"]
-            feels_like = tomorrow_weather["main"]["feels_like"]
-            weather_description_en = tomorrow_weather["weather"][0]["description"]
-            pressure = tomorrow_weather["main"]["pressure"]
-            wind_speed = tomorrow_weather["wind"]["speed"]
-            humidity = tomorrow_weather["main"]["humidity"]
 
-            # перевод слов
-            weather_description_ru = weather_mapping.get(weather_description_en, weather_description_en)
+            for weather_data in data["list"]:
+                forecast_datetime = datetime.datetime.utcfromtimestamp(weather_data["dt"])
 
-            # сообщение о погоде на завтра
-            weather_info = (f'🌤️Погода на завтра в {last_city.capitalize()}: {weather_description_ru}\n'
-                            f'🌡️Температура: {temp}°C\n'
-                            f'🤲Ощущается как: {feels_like}°C\n'
-                            f'💧Влажность: {humidity}%\n'
-                            f'🎈Давление: {pressure} мбар\n'
-                            f'🍃Скорость ветра: {wind_speed} м/с')
+                if forecast_datetime.date() == date.date():
+                    weather_description_en = weather_data["weather"][0]["description"]
+                    weather_description_ru = self.weather_mapping.get(weather_description_en, weather_description_en)
+                    temp = weather_data["main"]["temp"]
+                    feels_like = weather_data["main"]["feels_like"]
+                    humidity = weather_data["main"]["humidity"]
+                    pressure = weather_data["main"]["pressure"]
+                    wind_speed = weather_data["wind"]["speed"]
 
-            bot.reply_to(message, weather_info)
+                    weather_info = (f'🌤️ Погода: {weather_description_ru}\n'
+                                    f'🌡️ Температура: {temp}°C\n'
+                                    f'🤲 Ощущается как: {feels_like}°C\n'
+                                    f'💧 Влажность: {humidity}%\n'
+                                    f'🎈 Давление: {pressure} мбар\n'
+                                    f'🍃 Скорость ветра: {wind_speed} м/с')
+                    return weather_info
+        return None
+
+    # скан
+    def send_weather(self, chat_id, city):
+        weather_info = self.get_weather_for_date(city, datetime.datetime.now())
+        if weather_info:
+            self.bot.send_message(chat_id, weather_info, reply_markup=self.markup)
         else:
-            bot.reply_to(message, f'Не удалось получить погоду для {last_city}... 🥺')
-    else:
-        bot.reply_to(message, 'Вы еще не указали город. Сначала укажите город.')
+            self.bot.send_message(chat_id, f'Не удалось получить погоду для {city}... 🥺')
 
+    # таймер на час
+    def send_weather_periodically(self, chat_id):
+        while self.sending_weather:
+            if self.last_city:
+                self.send_weather(chat_id, self.last_city)
+            time.sleep(3600)
 
-@bot.message_handler(func=lambda message: message.text.lower() == "погода на 3 дня")
-def get_weather_three_days(message):
-    global last_city  # хранение города
-
-    # если есть город в хранилище то выполняется
-    if last_city:
-        city = last_city
-        current_date = datetime.datetime.now()
-        tomorrow_date = current_date + datetime.timedelta(days=1)
-        day_after_tomorrow_date = current_date + datetime.timedelta(days=2)
-        third_day_date = current_date + datetime.timedelta(days=3)
-
-        # погода 3 дней
-        tomorrow_weather = get_weather_for_date(city, tomorrow_date)
-        day_after_tomorrow_weather = get_weather_for_date(city, day_after_tomorrow_date)
-        third_day_weather = get_weather_for_date(city, third_day_date)
-
-        if tomorrow_weather and day_after_tomorrow_weather and third_day_weather:
-            temp_tomorrow = tomorrow_weather["main"]["temp"]
-            temp_day_after_tomorrow = day_after_tomorrow_weather["main"]["temp"]
-            temp_third_day = third_day_weather["main"]["temp"]
-            weather_description_en_tomorrow = tomorrow_weather["weather"][0]["description"]
-            weather_description_en_day_after_tomorrow = day_after_tomorrow_weather["weather"][0]["description"]
-            weather_description_en_third_day = third_day_weather["weather"][0]["description"]
-
-            # перевод слов
-            weather_description_ru_tomorrow = weather_mapping.get(weather_description_en_tomorrow, weather_description_en_tomorrow)
-            weather_description_ru_day_a_tomorrow = weather_mapping.get(weather_description_en_day_after_tomorrow, weather_description_en_day_after_tomorrow)
-            weather_description_ru_3_day = weather_mapping.get(weather_description_en_third_day, weather_description_en_third_day)
-
-            # сообщение о погоде
-            weather_info = (f'🏙️Погода в {city.capitalize()}:\n\n'
-                            f'📅 Завтра:\n'
-                            f'🌤️Погода: {weather_description_ru_tomorrow}\n'
-                            f'🌡️Температура: {temp_tomorrow}°C\n\n'
-                            f'📅 Послезавтра:\n'
-                            f'🌤️Погода: {weather_description_ru_day_a_tomorrow}\n'
-                            f'🌡️Температура: {temp_day_after_tomorrow}°C\n\n'
-                            f'📅 Через 3 дня:\n'
-                            f'🌤️Погоды: {weather_description_ru_3_day}\n'
-                            f'🌡️Температура: {temp_third_day}°C\n')
-
-            bot.reply_to(message, weather_info)
-        else:
-            bot.reply_to(message, f'Не удалось получить погоду для {city}... 🥺')
-    else:
-        bot.reply_to(message, 'Вы еще не указали город. Пожалуйста, укажите город.')
-
-
-# клавиатура над чатом
-markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-markup.add(telebot.types.KeyboardButton("Присылать каждый час"))
-markup.add(telebot.types.KeyboardButton("Стоп"))
-markup.add(telebot.types.KeyboardButton("Погода на 3 дня"))
-markup.add(telebot.types.KeyboardButton("Завтра"))
-
-# рассылка погодывп
-@bot.message_handler(content_types=['text'])
-def get_weather(message):
-    global last_city, scheduled_message, sending_weather
-
-    if message.text == "Присылать каждый час":
-        if sending_weather:
-            bot.send_message(message.chat.id, "Рассылка уже активна.")
-        else:
-            sending_weather = True
-            last_city = None
-            scheduled_message = bot.send_message(message.chat.id, "Информация о погоде будет приходить каждый час. Нажмите 'Стоп', чтобы остановить.")
-            send_weather_periodically(message.chat.id)
-    elif message.text == "Стоп":
-        if sending_weather:
-            sending_weather = False
-            last_city = None
-            bot.send_message(message.chat.id, "Рассылка остановлена. Нажмите 'Присылать каждый час', чтобы возобновить.", reply_markup=markup)
-            if scheduled_message:
-                bot.delete_message(message.chat.id, scheduled_message.message_id)
-        else:
-            bot.send_message(message.chat.id, "Рассылка уже остановлена...")
-    else:
-        last_city = message.text.strip().lower()  # город последний
-        send_weather(message.chat.id)
-
-
-def send_weather(chat_id):
-    global last_city
-    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={last_city}&appid={API}&units=metric')
-    if res.status_code == 200:
-        data = json.loads(res.text)
-        temp = data["main"]["temp"]
-        feels_like = data["main"]["feels_like"]
-        weather_description_en = data["weather"][0]["description"]
-        pressure = data["main"]["pressure"]
-        wind_speed = data["wind"]["speed"]
-        humidity = data["main"]["humidity"]
-
-        # перевод слов
-        weather_description_ru = weather_mapping.get(weather_description_en, weather_description_en)
-
-        # сообщение о погоде
-        weather_info = (f'🌤️Сейчас погода: {weather_description_ru}\n'
-                        f'🌡️Температура: {temp}°C\n'
-                        f'🤲Ощущается как: {feels_like}°C\n'
-                        f'💧Влажность: {humidity}%\n'
-                        f'🎈Давление: {pressure} мбар\n'
-                        f'🍃Скорость ветра: {wind_speed} м/с')
-
-        image = 'sun.png' if temp > 15.0 else 'cloud.png'
-        with open(image, 'rb') as file:
-            bot.send_photo(chat_id, file, caption=weather_info, reply_markup=markup)
-    else:
-        bot.send_message(chat_id, f'Город указан неверно или я такой не знаю... 🥺')
-
-
-def send_weather_periodically(chat_id):
-    while sending_weather:
-        if last_city:
-            send_weather(chat_id)
-        time.sleep(3600)  # таймер 1 часпа
+    # запуск бота
+    def start_bot(self):
+        self.bot.polling(none_stop=True)
 
 
 if __name__ == "__main__":
-    bot.polling(none_stop=True)
-
-
-# чтобы бот не останавливался
-bot.polling(none_stop=True)
+    token = '5946513884:AAGkl2MPaJ5sK0BRYsbWkY6PV23JJBF1-aU'
+    api_key = '709e3cbe105d05f7bfdb27403929d3bc'
+    bot = WeatherBot(token, api_key)
+    bot.start_bot()
